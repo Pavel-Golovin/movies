2cbfe2390e7aba0ec4467cf681d41e056dda0416
Pavel <Golovin.Pavel@outlook.com>
2021-02-16 16:00:10 +0300

    Вынес логику получения тела запроса в отдельную функцию + поправил линтер

--- src/services/api-services.js
+++ src/services/api-services.js
@@ -1,24 +1,25 @@
-
 export default class ApiServices {
-  
-  apiBase = "https://api.themoviedb.org/3/";
-  
-  apiKey = "api_key=0ba2480afffac1ffe260d6bed0c6fb99";
-  
-  getMoviesBySearch = async (query, page = 1) => {
-    const url = `${this.apiBase}search/movie?${this.apiKey}&language=en-US&query=${query}&page=${page}&include_adult=false`
+  apiBase = 'https://api.themoviedb.org/3/';
+
+  apiKey = 'api_key=0ba2480afffac1ffe260d6bed0c6fb99';
+
+  getResponse = async (url) => {
     const response = await fetch(url);
-  
     if (!response.ok) {
       throw new Error(`The URL ${url} can not be fetched, received ${response.status}`);
     }
-    
     const body = await response.json();
-    
-    if (body.results.length === 0) {
+    return body;
+  };
+
+  getMoviesBySearch = async (query, page = 1) => {
+    const url = `${this.apiBase}search/movie?${this.apiKey}&language=en-US&query=${query}&page=${page}&include_adult=false`;
+    const response = await this.getResponse(url);
+
+    if (response.results.length === 0) {
       throw new Error(`Couldn't find the movie`);
     }
-    
-    return body;
-  }
+
+    return response;
+  };
 }
